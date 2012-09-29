6c7d3c85a1052ebc06be6242aed0a190250ae172
Raven G. Duran <me@imraven.com>
2012-09-29 18:28:24 +0800

    Added Initial Insert and Creat node functions

--- btree.c
+++ btree.c
@@ -1,4 +1,13 @@
 #include <stdio.h>
+#define MAX 100
+
+// B-Tree Node Structure
+struct node {
+    int value[MAX];
+    struct node *keys[MAX+1];
+    struct node *parent;
+    int keyCount;
+} *tRoot = NULL;
 
 // Menu functions
 void showMenu();
@@ -6,6 +15,10 @@ void doInsert();
 void doDelete();
 void doSearch();
 void doInOrder();
+
+// Tree Operations
+struct node* newNode(int value,struct node *parent);
+struct node* insertN(int value,struct node *root,struct node *parent);
 
 
 int main(){
@@ -15,6 +28,7 @@ int main(){
     return 0;
 }
 
+// Menus
 void showMenu(){
    system("cls"); 
    printf("      _____           _____              \n");
@@ -39,9 +53,16 @@ void showMenu(){
 }
 
 void doInsert(){
+     int value;
+     
      system("cls"); 
      printf("--- Insertion ---\n");
+     printf("Value: ");
+     scanf("%d",&value);
      
+     tRoot = insertN(value,tRoot,tRoot);
+     
+     printf("Press any key to continue...\n");
      getch();
      showMenu();
 }
@@ -50,6 +71,7 @@ void doDelete(){
      system("cls"); 
      printf("--- Deletion ---\n");
      
+     printf("Press any key to continue...\n");
      getch();
      showMenu();
 }
@@ -58,6 +80,7 @@ void doSearch(){
      system("cls"); 
      printf("--- Search ---\n");
      
+     printf("Press any key to continue...\n");
      getch();
      showMenu();
 }
@@ -65,7 +88,26 @@ void doSearch(){
 void doInOrder(){
      system("cls"); 
      printf("--- Printing ---\n");
+     printf("%d\n",tRoot->value[0]);
      
+     printf("Press any key to continue...\n");
      getch();
      showMenu();
 }
+
+// Operations
+struct node* newNode(int value,struct node *parent){
+       struct node *root = (struct node*)malloc(sizeof(struct node));
+       root->value[0] = value;
+       root->keyCount = 2;
+       root->parent = parent;
+       
+       return root;
+}
+
+struct node* insertN(int value,struct node *root,struct node *parent){
+       if (root == NULL){
+          return newNode(value,parent);
+       }
+}
+
